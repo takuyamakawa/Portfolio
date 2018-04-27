2463335354e1d80282bed5f8a15db9bd14608054
takuyamakawa <g2117052@fun.ac.jp>
2018-04-27 15:29:12 +0900

    紹介文、技術、言語のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426072156) do
+ActiveRecord::Schema.define(version: 20180427061813) do
 
   create_table "menus", force: :cascade do |t|
     t.text "title"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 20180426072156) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "purpose"
+    t.text "intro"
+    t.text "tech"
+    t.text "lang"
   end
 
 end

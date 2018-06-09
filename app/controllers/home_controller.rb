81f5fda730f3e1a218bf689298ccb3f634af8bc3
takuyamakawa <g2117052@fun.ac.jp>
2018-06-09 23:46:25 +0900

    関連リンク表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,5 +6,6 @@ class HomeController < ApplicationController
   def detail
     @menu = Menu.find_by(id: params[:id])
     @img = Image.where(relation_id: @menu.id)
+    @web = Web.where(relation_id: @menu.id)
   end
 end
